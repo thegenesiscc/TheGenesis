b19de6e457865432dbecb4709c873427e34cff67
thegenesis <dev@thegenesis.cc>
2025-02-26 19:54:31 +0800

    show menubar by mobile

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
-import { Menu, X, ChevronDown } from 'lucide-react';
+import React, { useState } from 'react';
+import { Menu, X } from 'lucide-react';
 import { useTranslation } from 'react-i18next';
 import LanguageSwitcher from './LanguageSwitcher';
 
 export default function Navbar() {
   const { t } = useTranslation();
+  const [isOpen, setIsOpen] = useState(false); // 控制抽屉的状态
+
+  const toggleDrawer = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
     <nav className="fixed w-full z-50 bg-[#31205c] bg-opacity-40 backdrop-blur-lg">
@@ -28,12 +33,34 @@ export default function Navbar() {
           
           <div className="flex items-center space-x-4">
             <LanguageSwitcher />
-            {/* <button className="bg-blue-600 px-4 py-2 rounded-full">
-              {t('wallet.connect')}
-            </button> */}
+            <button onClick={toggleDrawer} className="md:hidden">
+              <Menu />
+            </button>
           </div>
         </div>
       </div>
+
+      {/* 抽屉菜单 */}
+      {isOpen && (
+        <div className="fixed inset-0 z-40 backdrop-blur-lg" onClick={toggleDrawer}>
+          <div className="bg-[#31205c] rounded-lg shadow-lg w-full h-full flex flex-col relative">
+            <div className="flex justify-end absolute top-4 right-4">
+              <button onClick={toggleDrawer} className="text-white">
+                <X />
+              </button>
+            </div>
+            <div className="bg-[#31205c] flex flex-col items-center justify-center h-full w-full mt-16">
+              <div className="w-full border-t border-white" />
+              <a href="/" className="w-full text-lg font-normal text-white hover:opacity-80 mb-2 text-center py-1">{t('nav.home')}</a>
+              {/* 分割线 */}
+              <div className="w-full border-t border-white" />
+              <a href="/trade" className=" bg-[#31205c] w-full text-lg font-normal text-white hover:opacity-80 text-center py-1">{t('nav.about')}</a>
+
+            </div>
+
+          </div>
+        </div>
+      )}
     </nav>
   );
 }
