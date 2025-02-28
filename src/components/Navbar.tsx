efa29b83104bebf80dde584c3052ffb4ee9b43bc
thegenesis <dev@thegenesis.cc>
2025-02-28 15:29:36 +0800

    update navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,17 +4,27 @@ import { useTranslation } from 'react-i18next';
 import LanguageSwitcher from './LanguageSwitcher';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 import { useAccount } from 'wagmi'; // 从 wagmi 中引入 useAccount
+import { useLocation, useNavigate } from 'react-router-dom'; // 导入 useLocation 和 useNavigate
 
 
 export default function Navbar() {
   const { t } = useTranslation();
   const [isOpen, setIsOpen] = useState(false); // 控制抽屉的状态
   const [walletText, setWalletText] = useState('链接钱包'); // 默认状态
+  const { isConnected } = useAccount();
+  const location = useLocation(); // 获取当前路径
+  const navigate = useNavigate(); // 用于导航
 
   const toggleDrawer = () => {
     setIsOpen(!isOpen);
   };
-  const { isConnected, address } = useAccount();
+
+  const handleNavigation = (path) => {
+    // 检查当前路径是否与目标路径相同
+    if (location.pathname !== path) {
+      navigate(path); // 跳转到目标路径
+    }
+  };
 
   return (
     <nav className="fixed w-full z-50 bg-[#31205c] bg-opacity-40 backdrop-blur-lg">
@@ -29,14 +39,14 @@ export default function Navbar() {
               />
             </div>
             <div className="hidden md:flex items-center space-x-8">
-              <a href="/" className="text-lg font-normal text-white bg-gradient-to-r from-purple-400 via-pink-300 to-orange-200 bg-clip-text text-transparent hover:opacity-80">{t('nav.home')}</a>
-              <a href="/about" className="text-lg font-normal text-white bg-gradient-to-r from-purple-400 via-pink-300 to-orange-200 bg-clip-text text-transparent hover:opacity-80">{t('nav.about')}</a>
-              <a href="/gameplay" className="text-lg font-normal text-white bg-gradient-to-r from-purple-400 via-pink-300 to-orange-200 bg-clip-text text-transparent hover:opacity-80">{t('nav.how')}</a>
+              <a onClick={() => handleNavigation('/')} className="text-lg font-normal text-white bg-gradient-to-r from-purple-400 via-pink-300 to-orange-200 bg-clip-text text-transparent hover:opacity-80">{t('nav.home')}</a>
+              <a onClick={() => handleNavigation('/about')} className="text-lg font-normal text-white bg-gradient-to-r from-purple-400 via-pink-300 to-orange-200 bg-clip-text text-transparent hover:opacity-80">{t('nav.about')}</a>
+              <a onClick={() => handleNavigation('/gameplay')} className="text-lg font-normal text-white bg-gradient-to-r from-purple-400 via-pink-300 to-orange-200 bg-clip-text text-transparent hover:opacity-80">{t('nav.how')}</a>
               {/* <a href="/subscription" className="text-lg font-normal text-white bg-gradient-to-r from-purple-400 via-pink-300 to-orange-200 bg-clip-text text-transparent hover:opacity-80">{t('nav.presale')}</a> */}
               {/* <a href="/docs" className="text-lg font-normal text-white bg-gradient-to-r from-purple-400 via-pink-300 to-orange-200 bg-clip-text text-transparent hover:opacity-80">{t('nav.docs')}</a>  */}
-              <div >
+              <div>
                 {isConnected ? (
-                  <a href="/subscription" className="text-lg font-normal text-white bg-gradient-to-r from-purple-400 via-pink-300 to-orange-200 bg-clip-text text-transparent hover:opacity-80">{t('nav.mywallet')}</a>
+                  <a onClick={() => handleNavigation('/subscription')} className="text-lg font-normal text-white bg-gradient-to-r from-purple-400 via-pink-300 to-orange-200 bg-clip-text text-transparent hover:opacity-80">{t('nav.mywallet')}</a>
                   // <span className="text-white">{t('nav.mywallet')}</span> // 显示连接状态和地址
                 ) : (
                   <span className="text-white"></span> // 显示未连接状态
@@ -69,15 +79,15 @@ export default function Navbar() {
             </div>
             <div className="relative bg-[#31205c] flex flex-col items-center justify-start h-full w-full pt-12">
               <div className="w-full border-t border-white" />
-              <a href="/" className="w-full text-lg font-normal text-white hover:opacity-80 text-center py-1 bg-[#31205c]">
+              <a onClick={() => handleNavigation('/')} className="w-full text-lg font-normal text-white hover:opacity-80 text-center py-1 bg-[#31205c]">
                 {t('nav.home')}
               </a>
               <div className="w-full border-t border-white" />
-              <a href="/about" className="bg-[#31205c] w-full text-lg font-normal text-white hover:opacity-80 text-center py-1">
+              <a onClick={() => handleNavigation('/about')} className="bg-[#31205c] w-full text-lg font-normal text-white hover:opacity-80 text-center py-1">
                 {t('nav.about')}
               </a>
               <div className="w-full border-t border-white" />
-              <a href="/gameplay" className="bg-[#31205c] w-full text-lg font-normal text-white hover:opacity-80 text-center py-1">
+              <a onClick={() => handleNavigation('/gameplay')} className="bg-[#31205c] w-full text-lg font-normal text-white hover:opacity-80 text-center py-1">
                 {t('nav.how')}
               </a>
               <div className="w-full border-t border-white" />
@@ -90,7 +100,7 @@ export default function Navbar() {
               </a> */}
               <div className="bg-[#31205c] w-full flex items-center space-x-4">
                 {isConnected ? (
-                  <a href="/subscription" className="bg-[#31205c] w-full text-lg font-normal text-white hover:opacity-80 text-center py-1">
+                  <a onClick={() => handleNavigation('/subscription')} className="bg-[#31205c] w-full text-lg font-normal text-white hover:opacity-80 text-center py-1">
                     {t('nav.mywallet')}
                   </a>
                 ) : (
